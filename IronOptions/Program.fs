4aab7af630ce2612335c461afc331a540b6ee7d1
kjchiu <krypes+github@gmail.com>
2012-06-11 19:51:04 -0400

    init.. gist + some modifications

---
+++ IronOptions/Program.fs
@@ -0,0 +1,42 @@
+﻿module IronOptions
+open FParsec
+open Util
+type UserState = unit
+type Parser<'a> = Parser<'a, UserState>
+
+type ScalarAst =
+    | Int of int32
+    | Float of float
+    | Boolean of bool
+    | String of string
+
+let option_delim : Parser<_>  = pstring "--"
+
+let flag s = option_delim >>? pstring s |>> (fun name-> (name, Boolean(true)))
+
+let string_literal = 
+    let validChar = satisfy (fun c -> c <> '\\' && c <> '"')
+    between (str "\"") (str "\"") (manyChars validChar)
+    
+let scalar_option (name) : Parser<string*ScalarAst>=
+    let scalar_option_p parser type_convert =
+        name |> flag |> p_ws .>>.? parser
+        |>> (fun (_flag, _value) -> 
+            let _name, _ = _flag
+            (_name, type_convert _value))
+    scalar_option_p pfloat Float
+    <|> scalar_option_p string_literal String 
+
+let option name = scalar_option name
+                  <|> flag name 
+
+let options names = 
+    let gen_parser name =  option name |> p_ws <!> name
+    let first = Seq.head names
+    many (Seq.fold (<|>)  (gen_parser first)  (Seq.map  gen_parser (Seq.skip 1 names)))
+
+test (options ["a"; "b"; "c"]) "--b 0.5 --a --a 123 --b \"hello there\""
+
+
+
+
---
+++ IronOptions/Util.fs
@@ -0,0 +1,33 @@
+﻿module Util
+open FParsec
+open System.Diagnostics
+
+let debugf fmt =
+    Printf.ksprintf (fun s -> Trace.Write(s)) fmt
+let debugfn fmt =
+    Printf.ksprintf (fun s -> Trace.WriteLine(s)) fmt
+
+let str = pstring 
+let ws = spaces
+let p_ws p = p <|> p .>> ws
+let str_ws s = pstring s .>> spaces
+#if VERBOSE_DEBUG
+let (<!>) (p: Parser<_,_>) label : Parser<_,_> =
+    fun stream ->
+        debugfn "%A: Entering %s" stream.Position label
+        let reply = p stream
+        debugfn "%A: Leaving %s (%A)(%c)" stream.Position label reply.Status (stream.Peek())
+        reply
+#else
+let (<!>) p label =
+    fun strm ->
+        p strm
+#endif
+        
+
+ 
+let test p str =
+    debugfn "test: %s" str
+    match run p str with
+    | Success(result, _, _)   -> debugfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> debugfn "Failure: %s" errorMsg
